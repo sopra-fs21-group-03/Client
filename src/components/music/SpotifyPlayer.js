ad045a76ea333a87d5d212691eb28fdcf08dd460
Luca Huber <luca.huber2@uzh.ch>
2021-05-17 15:32:06 +0200

    remove script cache

--- src/components/music/SpotifyPlayer.js
+++ src/components/music/SpotifyPlayer.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { ScriptCache } from "./ScriptCache";
 import SpotifyAuthWindow from "./SpotifyAuthWindow"
 
 class SpotifyPlayer extends Component {
